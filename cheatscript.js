b44ef4ef40aab23fda8ffd774d46a9d720d0b481
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-07-24 00:32:42 +0800

    Update cheatscript.js

--- cheatscript.js
+++ cheatscript.js
@@ -1,19 +1,10 @@
-var cheat = function () {
-  var iBox = document.querySelector('input[type="text"]');
-  var eMsg = document.querySelector('div[role="alert"]');
-//   if field is empty, input 000 and submit
-  if (iBox.value.length === 0) {
-    iBox.parentElement.lastChild.hidden = true;
-    iBox.value = 0;
-  }
-//   while field value not right, input value + 1
-  while (eMsg.innerHTML.length !== 0 && iBox.value != 10000) {
-    iBox.value += 1;
-  }
-}
-
-chrome.extension.onMessage.addListener(function (message, sender, callback) {
-    if (message.functiontoInvoke == "cheat") {
-        cheat();
-    }
+chrome.runtime.onMessage.addListener(function (request) {
+    var iBox = document.querySelector('input[type="text"]');
+    var elem = document.querySelector('div[data-params]')
+    var data = elem.getAttribute('data-params');
+    var text = data.match(/\[\"\d+\"\]/)[0];
+    text = text.replace( /\D/g, '');
+    iBox.value = '';
+    iBox.focus();
+    document.execCommand('insertText', false, text);
 });
